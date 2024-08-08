08a65bab8f4b3e47c35c85e4b765865504efe699
Sidhika <sidhika.t@outlook.com>
2024-08-08 19:13:24 -0400

    hard-coded approach to get around .env errors for server login

--- schedule-app/server/db/connection.js
+++ schedule-app/server/db/connection.js
@@ -3,8 +3,8 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 // dotenv.config();
 
 // Replace the uri string with config env.
-const uri = process.env.ATLAS_URI;
-// const uri = "mongodb+srv://admin:mWu2vMHAQfcDMyay@boba-talks.8ltm5.mongodb.net/?retryWrites=true&w=majority&appName=boba-talks";
+// const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://admin:mWu2vMHAQfcDMyay@boba-talks.8ltm5.mongodb.net/?retryWrites=true&w=majority&appName=boba-talks";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
